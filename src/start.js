6968d0f3d7ecd3f7db2a706d4f1b4be0ef125ce4
Carl Beerwerth <carl.beerwerth@gmail.com>
2019-07-23 14:02:29 +0200

    save

--- src/start.js
+++ src/start.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import AnimalsContainer from "./AnimalsContainer"; //no curly braces bc of default
+import App from "./App";
 
-ReactDOM.render(
-    <HelloWorld />,
-    document.querySelector('main')
-);
+ReactDOM.render(<App />, document.querySelector("main"));
 
-function HelloWorld() {
-    return (
-        <div>Hello, World!</div>
-    );
+export default function HelloWorld() {
+    return <div>Hello, Girl!</div>;
 }
